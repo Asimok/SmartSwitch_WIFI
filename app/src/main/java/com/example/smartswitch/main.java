112fc3e41432622a400fc6a87ec27564b2011667
LeoMachine <38159292+LeoMachine@users.noreply.github.com>
2019-05-04 19:54:24 +0800

    语音模块

--- app/src/main/java/com/example/smartswitch/main.java
+++ app/src/main/java/com/example/smartswitch/main.java
@@ -1,8 +1,12 @@
 package com.example.smartswitch;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,8 +17,11 @@ public class main extends Activity {
         setContentView(R.layout.main);
         Button jixie=findViewById(R.id.jixie);
         Button smart=findViewById(R.id.smart);
+        Button yuyin=findViewById(R.id.yuyin);
         final Intent intent2=new Intent(this,BluetoothClientActivity_mytimer_auto.class);
         final Intent intent1=new Intent(this,BluetoothClientActivity_mytimer_machinery.class);
+        final Intent intent3=new Intent(this,IatDemo.class);
+        requestPermissions();
         jixie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,10 +32,38 @@ public class main extends Activity {
         smart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(int ent2);
+                startActivity(intent2);
                 finish();
             }
         });
+        yuyin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent3);
+                finish();
+            }
+        });
+    }
+    private void requestPermissions(){
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                int permission = ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if(permission!= PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,new String[] {
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.LOCATION_HARDWARE,Manifest.permission.READ_PHONE_STATE,
+                            Manifest.permission.WRITE_SETTINGS,Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.RECORD_AUDIO,Manifest.permission.READ_CONTACTS},0x0010);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
